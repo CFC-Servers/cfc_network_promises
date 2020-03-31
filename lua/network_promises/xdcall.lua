f35a9ae6439f08e8a10f3b7a3cf66b3f6d16f7a6
Samuel Williams <33094920+samuelWilliams99@users.noreply.github.com>
2020-03-31 01:16:21 +0100

    Style fixes

--- lua/network_promises/xdcall.lua
+++ lua/network_promises/xdcall.lua
@@ -1,14 +1,16 @@
-
 -- Same as pcall, but returns error message and traceback on error
 function xdcall( func, ... )
     local traceback
     local data = { xpcall( func, function( err )
-        traceback = debug.traceback() 
+        traceback = debug.traceback()
         return err
     end, ... ) }
+
     local success = table.remove( data, 1 )
+
     if success then
         return true, unpack( data )
     end
+
     return false, data[1], traceback
 end
